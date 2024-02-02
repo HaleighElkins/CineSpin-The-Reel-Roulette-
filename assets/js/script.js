80421bec58b18255d0924113aa7e83dee3aa7ed7
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-02-01 20:42:45 -0700

    corrected TMDb Api Fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 
 
-// TMDb API This is what they gave me for the request. H.E
-OkHttpClient client = new OkHttpClient();
+// TMDb API fetch H.E
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODY0Njc0ZTgyZTM0ZmI2OWZmY2M1OWU5YWE4MTE1MiIsInN1YiI6IjY1YmIwZjgxMWZkMzZmMDE3ZDcyNTBjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bMg42C8e5ddc38-ZLr6hNsMSxXlHspaELs7ubZBQImo'
+  }
+};
 
-Request request = new Request.Builder()
-  .url("https://api.themoviedb.org/3/authentication")
-  .get()
-  .addHeader("accept", "application/json")
-  .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODY0Njc0ZTgyZTM0ZmI2OWZmY2M1OWU5YWE4MTE1MiIsInN1YiI6IjY1YmIwZjgxMWZkMzZmMDE3ZDcyNTBjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bMg42C8e5ddc38-ZLr6hNsMSxXlHspaELs7ubZBQImo")
-  .build();
-
-Response response = client.newCall(request).execute();
+fetch('https://api.themoviedb.org/3/authentication', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
