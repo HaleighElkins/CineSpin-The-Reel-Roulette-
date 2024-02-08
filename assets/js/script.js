33230a60b47baecaf50e2628076ff62de10011ca
goofy foot <williamk31@gmail.com>
2024-02-07 19:46:59 -0700

    added spinner to modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,38 @@ var genreBtn = document.querySelector(".buttons")
 var userChoice = "";
 genreBtn.addEventListener('click', function(event){
     if(event.target.tagName === "BUTTON"){
-    console.log(event.target.id); 
+    console.log(event.target); 
     userChoice = event.target.id;
     document.getElementById("user-choice").textContent = userChoice;
     }
 });
 
 document.querySelector(".is-success").addEventListener('click', function(event){
-  document.location.replace("spinner.html")
+  var pageNumber = Math.floor(Math.random() * (10 - 1));
+  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page='+ pageNumber +'&sort_by=popularity.desc&with_genres=' + userChoice, options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    // .then(response => queryResult[response])
+    .catch(err => console.error(err));
+  // document.location.replace("spinner.html")
 })
 
-// TMDb API fetch H.E
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODY0Njc0ZTgyZTM0ZmI2OWZmY2M1OWU5YWE4MTE1MiIsInN1YiI6IjY1YmIwZjgxMWZkMzZmMDE3ZDcyNTBjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bMg42C8e5ddc38-ZLr6hNsMSxXlHspaELs7ubZBQImo'
-  }
-};
 
-fetch('https://api.themoviedb.org/3/authentication', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+
+
+
+//TMDb API fetch H.E
+// var options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODY0Njc0ZTgyZTM0ZmI2OWZmY2M1OWU5YWE4MTE1MiIsInN1YiI6IjY1YmIwZjgxMWZkMzZmMDE3ZDcyNTBjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bMg42C8e5ddc38-ZLr6hNsMSxXlHspaELs7ubZBQImo'
+//   }
+// };
+
+// fetch('https://api.themoviedb.org/3/authentication', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
 
  
