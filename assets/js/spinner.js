44cc82e6050cd4a10dcd696dd5bd22150eba881e
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-02-08 19:25:22 -0700

    added in extra code

--- assets/js/spinner.js
+++ assets/js/spinner.js
@@ -7,9 +7,8 @@ var padding = { top: 20, right: 40, bottom: 0, left: 0 },
     picked = 100000,
     oldpick = [],
     color = d3.scale.category20();//category20c()
-    // -----------------Start of edit----------------------------------------
-//randomNumbers = getRandomNumbers();
-//http://osric.com/bingo-card-generator/?title=HTML+and+CSS+BINGO!&words=padding%2Cfont-family%2Ccolor%2Cfont-weight%2Cfont-size%2Cbackground-color%2Cnesting%2Cbottom%2Csans-serif%2Cperiod%2Cpound+sign%2C%EF%B9%A4body%EF%B9%A5%2C%EF%B9%A4ul%EF%B9%A5%2C%EF%B9%A4h1%EF%B9%A5%2Cmargin%2C%3C++%3E%2C{+}%2C%EF%B9%A4p%EF%B9%A5%2C%EF%B9%A4!DOCTYPE+html%EF%B9%A5%2C%EF%B9%A4head%EF%B9%A5%2Ccolon%2C%EF%B9%A4style%EF%B9%A5%2C.html%2CHTML%2CCSS%2CJavaScript%2Cborder&freespace=true&freespaceValue=Web+Design+Master&freespaceRandom=false&width=5&height=5&number=35#results
+    // ----------------Updated fetch for descriptions H.E  ----------------------------------------
+
 var data = [
     { "title": "", "Movie 1`": 1, "Overview 1": "" }, 
     { "title": "", "Movie 2'": 2, "Overview 2": "" }, 
@@ -70,13 +69,7 @@ const userSelectedGenreId = 35; // Replace with the actual genre ID
 updateSpinnerWithTmdbData(userSelectedGenreId);
 
 
-
-
-
-
-
-
-// --------------------------------------------END------------------------------------
+// --------------------------------------------END H.E ------------------------------------
 var svg = d3.select('#chart')
     .append("svg")
     .data([data])
