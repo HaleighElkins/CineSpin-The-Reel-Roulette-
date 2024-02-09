38f62e7ab1a999d24fcedbb96f77d9273d31dad8
goofy foot <williamk31@gmail.com>
2024-02-08 18:38:43 -0700

    tried to get the wheel to have text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //event listener for buttons W.K.
 var genreBtn = document.querySelector(".buttons")
 var userChoice = "";
+var movies = "";
 genreBtn.addEventListener('click', function(event){
     if(event.target.tagName === "BUTTON"){
     console.log(event.target); 
@@ -10,31 +11,25 @@ genreBtn.addEventListener('click', function(event){
 });
 
 document.querySelector(".is-success").addEventListener('click', function(event){
-  var pageNumber = Math.floor(Math.random() * (10 - 1));
-  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page='+ pageNumber +'&sort_by=popularity.desc&with_genres=' + userChoice, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    // .then(response => queryResult[response])
-    .catch(err => console.error(err));
-  // document.location.replace("spinner.html")
+  getMovies();
 })
 
+async function getMovies() {
+  var pageNumber = Math.floor(Math.random() * (10 - 1)+1);
+  var response = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=' + pageNumber + '&sort_by=popularity.desc&with_genres='+ userChoice, options);
+  movies = await response.json();
+  console.log(movies);
+  console.log(movies.results[0].title)
+  pushSpinner();
+}
 
+//function to push movie data into data variable
+function pushSpinner() {
+for (let i=0; i<10; i++){
+  data[i].label = (movies.results[i].title);
+}
+console.log(data);
+}
 
-
-
-//TMDb API fetch H.E
-// var options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODY0Njc0ZTgyZTM0ZmI2OWZmY2M1OWU5YWE4MTE1MiIsInN1YiI6IjY1YmIwZjgxMWZkMzZmMDE3ZDcyNTBjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bMg42C8e5ddc38-ZLr6hNsMSxXlHspaELs7ubZBQImo'
-//   }
-// };
-
-// fetch('https://api.themoviedb.org/3/authentication', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
 
  
